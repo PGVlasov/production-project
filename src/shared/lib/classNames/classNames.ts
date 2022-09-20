29ff8c786b86f5adca545888da9855223d4f4473
PGVlasov <PGVlasov@users.noreply.github.com>
2022-09-20 12:25:51 +0300

    Navbar added

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,12 +2,12 @@ type Mods = Record<string, boolean | string>;
 
 export function classNames(
   cls: string,
-  mods: Mods,
-  additionals: string[]
+  mods: Mods = {},
+  additionals: string[] = []
 ): string {
   return [
     cls,
-    ...additionals,
+    ...additionals.filter(Boolean),
     ...Object.entries(mods)
       .filter(([className, value]) => Boolean(value))
       .map(([className]) => className),
--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -1,14 +1,33 @@
-import { Link } from "react-router-dom";
 import classes from "./Navbar.module.scss";
 import { useTheme } from "../../../app/providers/ThemeProvider/lib/useTheme";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 
-export const Navbar = () => {
+interface NavbarProps {
+  className?: string;
+}
+
+export const Navbar = ({ className }: NavbarProps) => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <div className={classes.navbar}>
-      <Link to={"/"}>Главная страница</Link>
-      <Link to={"/about"}>О сайте</Link>
-      <button onClick={toggleTheme}>Сменить тему</button>
+    <div className={classNames(classes.Navbar, {}, [className])}>
+      <div className={classes.links}>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          className={classes.Link}
+          to={"/"}
+        >
+          Главная страница
+        </AppLink>
+        <AppLink
+          theme={AppLinkTheme.SECONDARY}
+          className={classes.Link}
+          to={"/about"}
+        >
+          О сайте
+        </AppLink>
+        <button onClick={toggleTheme}>Сменить тему</button>
+      </div>
     </div>
   );
 };
