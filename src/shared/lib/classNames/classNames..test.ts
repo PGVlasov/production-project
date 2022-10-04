db912b309662c359a0b5c9fcdabaee5bc3738e0f
PGVlasov <PGVlasov@users.noreply.github.com>
2022-10-04 20:59:14 +0300

    test fixed

--- src/shared/lib/classNames/classNames..test.ts
+++ src/shared/lib/classNames/classNames..test.ts
@@ -4,14 +4,23 @@ describe("classNames", () => {
   test("test with only first class", () => {
     expect(classNames("someClass")).toBe("someClass");
   });
+
   test("test with additional classes", () => {
     expect(classNames("someClass", {}, ["class1", "class2"])).toBe(
       "someClass class1 class2"
     );
   });
-  test("test with 2 mods true", () => {
-    expect(classNames("someClass")).toBe("someClass");
+
+  test("with mods", () => {
+    const expected = "someClass class1 class2 hovered scrollable";
+    expect(
+      classNames("someClass", { hovered: true, scrollable: true }, [
+        "class1",
+        "class2",
+      ])
+    ).toBe(expected);
   });
+
   test("test with 1 mode true and 1 md fasle", () => {
     expect(
       classNames("someClass", { hovered: true, scrollable: false }, [
@@ -20,6 +29,7 @@ describe("classNames", () => {
       ])
     ).toBe("someClass class1 class2 hovered");
   });
+
   test("test with 1 mode true and 1 md fasle", () => {
     expect(
       classNames("someClass", { hovered: true, scrollable: undefined }, [
