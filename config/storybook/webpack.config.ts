586537e8aeb9836efc60b80741478eda8741c6b1
PGVlasov <PGVlasov@users.noreply.github.com>
2022-10-04 22:59:14 +0300

    fix config

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -13,7 +13,6 @@ export default ({ config }: { config: webpack.Configuration }) => {
 
   config.resolve?.modules?.push(pathes.src);
   config.resolve?.extensions?.push(".ts", ".tsx");
-
   config.module.rules = config.module?.rules?.map((rule: RuleSetRule) => {
     if (/svg/.test(rule.test as string)) {
       return { ...rule, exclude: /\.svg$/i };
